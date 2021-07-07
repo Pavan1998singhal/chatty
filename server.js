921b955e5eb76ff8aa18e2f303760ecaa2774286
Pavan Singhal <spavan769@gmail.com>
2021-07-07 12:53:39 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+      }
+});
+
+io.on("connection", (socket) => {
+    console.log("What is socket: ", socket);
+    console.log("Socket is active to be connected !!");
+
+    // server is listening to an socket event that going to recieved by client
+    socket.on("chat", (payload) => {
+        console.log("What is payload: ", payload);
+
+        // respond to the recieved socket event
+        io.emit("chat", payload);
+    })
+})
+
+
+// this is not how we create a listening server on this.
+//app.listen(5000, () => console.log("server is running.."));
+
+server.listen(5000, () => {
+    console.log("Server is running at port 5000...");
+});
